d9262ed74cd302a21adc73b93ef21c8ecfd67bd0
i-aana <aanal.s.patel1705@gmail.com>
2026-05-20 08:16:48 +0530

    made create task and home endpoint

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from authentication!")
+from fastapi import FastAPI, Depends, HTTPException, status
+from database import TaskStorage, get_task_store
+from models import TaskCreate, TaskUpdate, TaskResponse
+
+app = FastAPI(title="Task Management API", version="1.0")
+
+@app.get("/", tags=["Home"])
+def home():
+    return {"message": "Task Management API Running"}
 
 
-if __name__ == "__main__":
-    main()
+@app.post("/tasks", response_model=TaskResponse, status_code=status.HTTP_201_CREATED, tags=["Tasks"], summary="Create new task")
+def create_task(task: TaskCreate, store: TaskStorage = Depends(get_task_store)):
+
+    new_task = {
+        "id": store.current_id,
+        "title": task.title,
+        "description": task.description,
+        "due_date": task.due_date
+    }
+
+    store.tasks.append(new_task)
+    store.current_id += 1
+
+    return new_task
+
